4050c14edcb100779ce39492fc503b5d280fb388
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-27 12:09:51 -0500

    Make AJAX request to root path and loop over candidates to display on index page

--- election.js
+++ election.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
-  // Imagination!
-
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(responseData){
+    var candidates = responseData.candidates;
+    for (var i = 0; i < candidates.length; i++) {
+      $('#candidates').append(
+        '<h1> Candidate: ',
+        '<li> Name: ' + candidates[i].name + '</li>',
+        '<li> Votes: ' + candidates[i].votes + '</li>');
+    }
+  });
 });
